823500c98270ebe7f50925714eb2644b0c457798
Devashwinatx <ashwinrajaashwin7@gmail.com>
2025-06-02 09:17:37 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,20 +3,25 @@ from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-
-
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>developer :</b> <a href='https://t.me/Dorahari'>Naruto</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://t.me/animetamil_xyz'>Vps</a> \n<b>📢 Channel :</b> <a href='https://t.me/animetamil_xyz'>Anime Tamil Xyz</a> \n<b>🧑‍💻 creater :</b> <a href='tg://user?id={OWNER_ID}'>Owner</a>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            text = (
+                "<blockquote>"
+                "<b>👨‍💻 Developer :</b> <a href='https://t.me/Dorahari'>Naruto</a>\n"
+                "<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>\n"
+                "<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {version}</a>\n"
+                "<b>🚀 Server :</b> <a href='https://t.me/animetamil_xyz'>VPS</a>\n"
+                "<b>📢 Channel :</b> <a href='https://t.me/animetamil_xyz'>Anime Tamil Xyz</a>\n"
+                "<b>🧑‍💻 Creator :</b> <a href='tg://user?id={owner_id}'>Owner</a>"
+                "</blockquote>"
+            ).format(version=__version__, owner_id=OWNER_ID),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
+                    [InlineKeyboardButton("🔒 Close", callback_data="close")]
                 ]
             )
         )
@@ -26,13 +31,3 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
